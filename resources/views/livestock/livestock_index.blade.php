a495304f127db56bce3b29f92e0409246263e62a
Balasooriya R.C <ravindu.chamara2000@gmail.com>
2025-06-23 14:36:42 +0530

    access control under user managment of the fingerlings, agriculture and livestock  modules.

--- resources/views/livestock/livestock_index.blade.php
+++ resources/views/livestock/livestock_index.blade.php
@@ -263,9 +263,9 @@
                     </td>
 
                     <td data-label="Actions">
-                        <a href="{{ route('livestocks.edit', ['beneficiary_id' => $beneficiary->id, 'livestock_id' => $livestock->id]) }}"
+                        <a href="{{ route('livestocks.edit', ['beneficiary_id' => $beneficiary->id, 'livestock' => $livestock->id]) }}"
                             class="btn btn-warning">Edit</a>
-                         
+
                         <form action="{{ route('livestocks.destroy', ['beneficiary_id' => $beneficiary->id, 'livestock_id' => $livestock->id]) }}"
                               method="POST" style="display:inline;">
                             @csrf
